0d5056daaa19c0bb5e0f5ccb3f820876d256c3fb
Carina Fan <carinalfan@gmail.com>
2021-04-08 21:08:59 -0600

    Loop runs up to i=11.

--- cleaning/complex1.R
+++ cleaning/complex1.R
@@ -43,24 +43,68 @@ for (i in 1:n) {
     append(which(raw_user_data$tag2 == "Task Switch")) %>% 
     append(which(raw_user_data$tag2 == "Game Ended")[1])
   
-  # set up participant dataframe
-  temp_df = matrix(data = NA, nrow = length(switches) - 1, ncol = length(names)) %>% 
-    data.frame()
+  # sometimes there are no task switches
+  # if so there needs to be a slightly different workflow 
+  if (!is.na(switches[2])) {
+    
+    # set up participant dataframe  
+    temp_df = matrix(data = NA, nrow = length(switches) - 1, ncol = length(names)) %>% 
+      data.frame()
+    
+    names(temp_df) = names
+    
+    # fill in task order
+    temp_df$order = seq(1:(length(switches)-1))
+    
+    # fill in task data
+    
+    for (s in 1:(length(switches) - 1)) {
+      
+      # subset task rows
+      task_range = (switches[s]:(switches[s+1]-1))
+      raw_task_data = raw_user_data[task_range, ]
+      
+      # pull duration
+      task_duration = raw_task_data$created_at[1] %>% 
+        interval(raw_task_data$created_at[nrow(raw_task_data)]) %>% 
+        time_length("seconds")
+      
+      # fill in participant dataframe
+      temp_df$duration[s] = task_duration
+      
+    }
+    
+    } else { 
+    
+    switches = switches[1]
+    
+    # set up participant dataframe
+    temp_df = matrix(data = NA, nrow = 1, ncol = length(names)) %>% 
+      data.frame()
+    
+    names(temp_df) = names
+    
+    # fill in task order
+    temp_df$order = 1
+    
+    # fill in task data
+    
+    # pull duration
+    task_duration = raw_user_data$created_at[1] %>% 
+      interval(raw_user_data$created_at[nrow(raw_user_data)]) %>% 
+      time_length("seconds")
+    
+    # fill in participant dataframe
+    temp_df$duration = task_duration
+      
+    }
   
-  names(temp_df) = names
-  
-  # fill in user ID, date, task order
+  # fill in user ID & date
   temp_df$user_id = raw_user_data$user_id[1]
   temp_df$date = raw_user_data$created_at[1] %>% 
     date()
-  temp_df$order = seq(1:(length(switches)-1))
-  
-  # fill in task data
-  
-  # for (s in 1:(length(switches) - 1)) {
-  #   
-  # }
   
   # append participant dataframe to overall dataframe
   df %<>% rbind(temp_df)
-}
+} 
+
